30a993c6c4bb5223d159c69e760717379546a9ae
Muhammad Talha Bin Ishaque <talha.i1997@gmail.com>
2022-02-19 21:58:31 +0500

    Testing

--- app/Http/Requests/InvoiceRequest.php
+++ app/Http/Requests/InvoiceRequest.php
@@ -27,6 +27,7 @@ class InvoiceRequest extends FormRequest
             'customer_name' =>'required|string',
             'booker_id' => 'required|string',
             'salesman_name' => 'required |string',
+            
             'product_id' => 'required|not_in:0',
         ];
         return $rule;
