1e8d920f52f230c54c73b7545de80746904e4011
Muhammad-Taha07 <imtaha999@hotmail.com>
2023-02-21 23:07:17 +0500

    Fixed hectic Alignment

--- resources/views/products/products.blade.php
+++ resources/views/products/products.blade.php
@@ -27,34 +27,25 @@
             <div class="col-12">
               <div class="card">
                 <div class="card-header">
-                  {{-- CODE HERE --}}                 
-                   <div class="col-md-5">
-                        
-                            <div class="row">
-                              <div class="input-group" style="width: 241px; align-content: left">
-                                <select name="category_id" class="form-control" id="category" required>
-                                  <option value="" selected="true">Select sasa</option>
-                                  {{-- @foreach($categories as $category)
-                                    <option value="{{ $category->id }}">{{$category->name}}</option>
-                                  @endforeach --}}
-                                </select>
-                              </div>
-                            
-                            <div class="input-group input-group-sm" style="width: 50px;">
-                              <a href="{{route('product/add-new-product')}}" title="Add New Product" class="btn btn-block btn-success"><i class="fas fa-plus"></i></a>
-                            </div>&nbsp
-                            <div class="input-group input-group-sm" style="width: 50px;">
-                            <button disabled type="button" value="Delete" id="deleteAllSelectedRecords" class="btn btn-danger check" style="width: 100%;"><i class="fas fa-trash-alt"></i></button>
-                            </div> 
-                          
-                            </div>
-                                            
-                         
-                        
-                        {{-- @error('category_id')
-                            <p style="color:red">{{ $message }}</p>
-                        @enderror --}}
-                   </div>
+                  {{-- CODE HERE --}}  
+                  <div class="input-group" style="width: 241px; float: left;">
+                    <select name="category_id" class="form-control" id="category" required>
+                      <option value="" selected="true">Select sasa</option>
+                      {{-- @foreach($categories as $category)
+                        <option value="{{ $category->id }}">{{$category->name}}</option>
+                      @endforeach --}}
+                      
+                    </select>
+                    @error('category_id')
+                    <p style="color:red">{{ $message }}</p>
+                    @enderror
+                  </div>
+                  <div class="input-group input-group-sm" style="width: 50px; float: right;">
+                    <a href="{{route('product/add-new-product')}}" title="Add New Product" class="btn btn-block btn-success"><i class="fas fa-plus"></i></a>
+                  </div>&nbsp
+                  <div class="input-group input-group-sm" style="width: 50px; float: right;">
+                  <button disabled type="button" value="Delete" id="deleteAllSelectedRecords" class="btn btn-danger check" style="width: 100%;"><i class="fas fa-trash-alt"></i></button>
+                  </div>                
                   {{-- CODE HERE --}}
            
                   {{-- <div class="card-tools">
