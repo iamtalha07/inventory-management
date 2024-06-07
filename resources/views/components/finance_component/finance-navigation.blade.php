b22edad23e83eefedf9915bb21a385a817c51c75
Muhammad Taha <imtaha999@hotmail.com>
2024-06-08 01:47:02 +0500

    view file for finance navigation component

---
+++ resources/views/components/finance_component/finance-navigation.blade.php
@@ -0,0 +1,56 @@
+<h5 class="mb-2">Reports Navigation |<small><i>Navigate to other reports from here</i></small></h5>
+<div class="row">
+  <div class="col-md-3 col-sm-6 col-12">
+    <a href="{{route('view-finances')}}"style="color: inherit;">
+    <div class="info-box shadow-none">
+      <span class="info-box-icon bg-info"><i class="far fa-copy"></i></span>
+      <div class="info-box-content">
+        <span class="info-box-text"></span>
+        <span class="info-box-number">Account Finance</span>
+      </div>
+ 
+    </div>
+    </a>
+    <!-- /.info-box -->
+  </div>
+  <!-- /.col -->
+  <div class="col-md-3 col-sm-6 col-12">
+    <div class="info-box shadow-sm">
+      <span class="info-box-icon bg-success"><i class="far fa-flag"></i></span>
+
+      <div class="info-box-content">
+        <span class="info-box-text">Shadows</span>
+        <span class="info-box-number">Small</span>
+      </div>
+      <!-- /.info-box-content -->
+    </div>
+    <!-- /.info-box -->
+  </div>
+  <!-- /.col -->
+  <div class="col-md-3 col-sm-6 col-12">
+    <div class="info-box shadow">
+      <span class="info-box-icon bg-warning"><i class="far fa-copy"></i></span>
+
+      <div class="info-box-content">
+        <span class="info-box-text">Shadows</span>
+        <span class="info-box-number">Regular</span>
+      </div>
+      <!-- /.info-box-content -->
+    </div>
+    <!-- /.info-box -->
+  </div>
+  <!-- /.col -->
+  <div class="col-md-3 col-sm-6 col-12">
+    <div class="info-box shadow-lg">
+      <span class="info-box-icon bg-danger"><i class="far fa-star"></i></span>
+
+      <div class="info-box-content">
+        <span class="info-box-text">Shadows</span>
+        <span class="info-box-number">Large</span>
+      </div>
+      <!-- /.info-box-content -->
+    </div>
+    <!-- /.info-box -->
+  </div>
+  <!-- /.col -->
+</div>
