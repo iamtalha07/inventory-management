ef9c59f0028c13cf46b1e4a30396ef97751f7dc3
Muhammad Taha <imtaha999@hotmail.com>
2024-07-02 12:08:40 +0500

    Balance feature with redirection added after login

--- app/Http/Controllers/Auth/LoginController.php
+++ app/Http/Controllers/Auth/LoginController.php
@@ -2,9 +2,11 @@
 
 namespace App\Http\Controllers\Auth;
 
+use App\BrandBalance;
 use App\Http\Controllers\Controller;
 use App\Providers\RouteServiceProvider;
 use Illuminate\Foundation\Auth\AuthenticatesUsers;
+use Illuminate\Http\Request;  // Ensure this is Illuminate\Http\Request
 
 class LoginController extends Controller
 {
@@ -36,5 +38,14 @@ class LoginController extends Controller
     public function __construct()
     {
         $this->middleware('guest')->except('logout');
+    }
+
+    protected function authenticated(Request $request, $user)
+    {
+        $balanceExists = BrandBalance::where('balance', '>', 0)->exists();
+
+        if ($balanceExists) {
+            session()->flash('warning', 'You have Outstanding Balance!');
+        }
     }
 }
